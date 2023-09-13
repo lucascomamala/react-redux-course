929d94f55d78817fdd008736902b71a3c0cdf7cc
Lucas <lucascomamala@gmail.com>
2023-09-13 15:40:52 -0600

    38 - A Big Pile of HTML

--- pdas/src/ProfileCard.jsx
+++ pdas/src/ProfileCard.jsx
@@ -1,9 +1,18 @@
 const ProfileCard = ({ title, handle, image }) => {
   return (
-    <div>
-      <img src={image} alt='pda logo' />
-      <div>Title is {title}</div>
-      <div>Handle is {handle}</div>
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={image} alt='pda logo' />
+        </figure>
+      </div>
+      
+      <div className="card-content">
+        <div className="media-content">
+          <p className="title is-4">Title is {title}</p>
+          <p className="subtitle is-4">Handle is {handle}</p>
+        </div>
+      </div>
     </div>
   )
 }
