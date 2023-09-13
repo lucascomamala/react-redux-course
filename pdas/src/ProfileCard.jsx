32f2f405736ec2501bb99fa7bf7aec069d633aba
Lucas <lucascomamala@gmail.com>
2023-09-13 15:44:58 -0600

    39 - Last Bit of Styling

--- pdas/src/ProfileCard.jsx
+++ pdas/src/ProfileCard.jsx
@@ -1,4 +1,4 @@
-const ProfileCard = ({ title, handle, image }) => {
+const ProfileCard = ({ title, handle, image, description }) => {
   return (
     <div className="card">
       <div className="card-image">
@@ -9,9 +9,10 @@ const ProfileCard = ({ title, handle, image }) => {
       
       <div className="card-content">
         <div className="media-content">
-          <p className="title is-4">Title is {title}</p>
-          <p className="subtitle is-4">Handle is {handle}</p>
+          <p className="title is-4">{title}</p>
+          <p className="subtitle is-6">{handle}</p>
         </div>
+        <div className="content">{description}</div>
       </div>
     </div>
   )
