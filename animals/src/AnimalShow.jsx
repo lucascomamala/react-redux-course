dc7e0c2cf466269a9a97b926dcdd01ea5140935d
Lucas <lucascomamala@gmail.com>
2023-09-14 10:06:45 -0600

    56 - Loading and Showing SVGs

--- animals/src/AnimalShow.jsx
+++ animals/src/AnimalShow.jsx
@@ -1,6 +1,25 @@
+import bird from './svg/bird.svg'
+import cat from './svg/cat.svg'
+import dog from './svg/dog.svg'
+import gator from './svg/gator.svg'
+import horse from './svg/horse.svg'
+import cow from './svg/cow.svg'
+import heart from './svg/heart.svg'
+
+const svgMap = {
+  bird,
+  cat,
+  dog,
+  gator,
+  horse,
+  cow,
+}
+
 const AnimalShow = ({ type }) => {
   return (
-    <div>{type}</div>
+    <div>
+      <img src={svgMap[type]} alt='animal' />
+    </div>
   )
 }
 
