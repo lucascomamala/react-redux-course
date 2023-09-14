da82c73632eaf5584b2e21c1c80549e6cc690f82
Lucas <lucascomamala@gmail.com>
2023-09-14 10:09:56 -0600

    57 - Increasing Image Size

--- animals/src/AnimalShow.jsx
+++ animals/src/AnimalShow.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import bird from './svg/bird.svg'
 import cat from './svg/cat.svg'
 import dog from './svg/dog.svg'
@@ -16,9 +18,20 @@ const svgMap = {
 }
 
 const AnimalShow = ({ type }) => {
+  const [clicks, setClicks] = useState(0)
+
+  const handleClick = () => {
+    setClicks(clicks + 1)
+  }
+
   return (
-    <div>
+    <div onClick={handleClick}>
       <img src={svgMap[type]} alt='animal' />
+      <img
+        src={heart}
+        alt='heart'
+        style={{ width: 10 + 10 * clicks }}
+      />
     </div>
   )
 }
