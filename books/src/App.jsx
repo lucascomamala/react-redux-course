e7ba2040168d9e4f4dd415d418418bc068ab5e28
Lucas <lucascomamala@gmail.com>
2023-09-15 17:11:32 -0600

    130 - Deleting a Record

--- books/src/App.jsx
+++ books/src/App.jsx
@@ -42,7 +42,9 @@ function App() {
     setBooks(updatedBooks)
   }
 
-  const deleteBookById = (id) => {
+  const deleteBookById = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`)
+    
     const updatedBooks = books.filter(book => book.id !== id)
     setBooks(updatedBooks)
   }
