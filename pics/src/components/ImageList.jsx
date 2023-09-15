cfc690729d8c3a81f4c25c642d5ed48492e53be5
Lucas <lucascomamala@gmail.com>
2023-09-14 18:16:13 -0600

    81 - Notes on Keys

--- pics/src/components/ImageList.jsx
+++ pics/src/components/ImageList.jsx
@@ -2,7 +2,7 @@ import ImageShow from './ImageShow'
 
 const ImageList = ({ images }) => {
   const renderedImages = images.map((image) => {
-    return <ImageShow image={image} />
+    return <ImageShow key={image.id} image={image} />
   })
 
   return (
