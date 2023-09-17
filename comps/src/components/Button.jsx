8b7148fa2e7dc0b84257604e25562cf3d007339b
Lucas <lucascomamala@gmail.com>
2023-09-17 13:30:34 -0600

    164 - The ClassNames Library

--- comps/src/components/Button.jsx
+++ comps/src/components/Button.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types'
+import classnames from 'classnames'
 
 const Button = ({
   children,
@@ -11,7 +12,7 @@ const Button = ({
   rounded
 }) => {
   return (
-    <button className='px-3 py-1.5 border border-blue-600 bg-blue-500 text-white'>
+    <button className=''>
       {children}
     </button>
   )
