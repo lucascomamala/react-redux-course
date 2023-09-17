ef140437219c0c9366915f2018c589184d8927a9
Lucas <lucascomamala@gmail.com>
2023-09-17 13:38:23 -0600

    165 - Building Some Variations

--- comps/src/components/Button.jsx
+++ comps/src/components/Button.jsx
@@ -11,8 +11,17 @@ const Button = ({
   outline,
   rounded
 }) => {
+  const classes = classnames('px-3 py-1.5 border text-white',{
+    'border-blue-500 bg-blue-500': primary,
+    'border-gray-900 bg-gray-900': secondary,
+    'border-green-500 bg-green-500': success,
+    'border-yellow-500 bg-yellow-500': warning,
+    'border-red-500 bg-red-500': danger,
+    'rounded-full': rounded,
+  })
+
   return (
-    <button className=''>
+    <button className={classes}>
       {children}
     </button>
   )
