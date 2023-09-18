48aa2a18e883811018bd759c86cfb50d9662ad90
Lucas <lucascomamala@gmail.com>
2023-09-17 19:19:05 -0600

    172 - Refactoring with Organization

---
+++ comps/src/pages/ButtonPage.jsx
@@ -0,0 +1,44 @@
+import { GoBell, GoTerminal, GoDatabase } from 'react-icons/go'
+
+import Button from '../components/Button'
+
+function ButtonPage() {
+  const handleClick = () => {
+    alert('You clicked me!')
+  }
+
+  return (
+    <div>
+      <div>
+        <Button className={'mb-5'} success rounded onClick={handleClick}>
+          <GoTerminal />
+          Click me!!
+        </Button>
+      </div>
+      <div>
+        <Button danger onClick={() => console.log('hello')}>
+          <GoDatabase />
+          Buy Now!
+        </Button>
+      </div>
+      <div>
+        <Button warning onMouseEnter={() => { console.log("enter") }}>
+          <GoBell />
+          See Deal!
+        </Button>
+      </div>
+      <div>
+        <Button secondary outline onMouseLeave={() => { console.log("leave") }}>
+          Hide Ads!
+        </Button>
+      </div>
+      <div>
+        <Button primary rounded>
+          Something!
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default ButtonPage
