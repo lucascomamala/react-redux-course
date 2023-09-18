01cbe7c947f22f8900b6e15bd9a2230f7078e8ef
Lucas <lucascomamala@gmail.com>
2023-09-17 21:38:34 -0600

    180 - Inline Event Handlers

--- comps/src/components/Accordion.jsx
+++ comps/src/components/Accordion.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 
 const Accordion = ({ items }) => {
-  const [expandedIndex, setExpandedIndex] = useState(2)
+  const [expandedIndex, setExpandedIndex] = useState(0)
 
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex
 
     return (
       <div key={item.id}>
-        <div>
+        <div onClick={() => setExpandedIndex(index)}>
           {item.label}
         </div>
         {isExpanded && <div>{item.content}</div>}
