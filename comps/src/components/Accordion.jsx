c46325fd3094cbb2fbc5ee9760d51c4735c2b5d3
Lucas <lucascomamala@gmail.com>
2023-09-17 22:45:17 -0600

    184 - Adding Styling

--- comps/src/components/Accordion.jsx
+++ comps/src/components/Accordion.jsx
@@ -11,22 +11,22 @@ const Accordion = ({ items }) => {
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex
 
-    const icon = <span>
+    const icon = <span className='text-2xl'>
       {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
     </span>
 
     return (
       <div key={item.id}>
-        <div onClick={() => handleClick(index)}>
-          {icon}
+        <div onClick={() => handleClick(index)} className='flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer'>
           {item.label}
+          {icon}
         </div>
-        {isExpanded && <div>{item.content}</div>}
+        {isExpanded && <div className='border-b p-5'>{item.content}</div>}
       </div>
     )
   })
   return (
-    <div>{renderedItems}</div>
+    <div className='border-x border-t rounded'>{renderedItems}</div>
   )
 }
 
