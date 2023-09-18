887c1a0452f51541dd5b908e88345b8df985a989
Lucas <lucascomamala@gmail.com>
2023-09-17 22:10:07 -0600

    181 - Variation on Event Handlers

--- comps/src/components/Accordion.jsx
+++ comps/src/components/Accordion.jsx
@@ -3,12 +3,16 @@ import { useState } from 'react'
 const Accordion = ({ items }) => {
   const [expandedIndex, setExpandedIndex] = useState(0)
 
+  const handleClick = (index) => {
+    setExpandedIndex(index)
+  }
+
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex
 
     return (
       <div key={item.id}>
-        <div onClick={() => setExpandedIndex(index)}>
+        <div onClick={() => handleClick(index)}>
           {item.label}
         </div>
         {isExpanded && <div>{item.content}</div>}
