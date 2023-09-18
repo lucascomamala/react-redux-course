fd8fafb76944177eac35215d8f1e35e28abab9ef
Lucas <lucascomamala@gmail.com>
2023-09-18 14:17:05 -0600

    201 - Adding Styling

--- comps/src/components/Dropdown.jsx
+++ comps/src/components/Dropdown.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { GoChevronDown } from "react-icons/go"
 
 const Dropdown = ({ options, value, onChange }) => {
   const [isOpen, setIsOpen] = useState(false)
@@ -14,21 +15,27 @@ const Dropdown = ({ options, value, onChange }) => {
 
   const renderedOptions = options.map((option) => {
     return (
-      <div key={option.value} className="item" onClick={() => handleOptionClick(option)}>
+      <div key={option.value} className="hover:bg-sky-100 rounded cursor-pointer p-1" onClick={() => handleOptionClick(option)}>
         {option.label}
       </div>
     )
   })
 
   return (
-    <>
-      <div onClick={handleToggleOpen}>
+    <div className="w-48 relative">
+      <div
+        className="flex justify-between items-center cursor-pointer border rounded p-3 bg-white w-full"
+        onClick={handleToggleOpen}
+      >
         {value?.label || 'Select...'}
+        <GoChevronDown className="text-xl" />
       </div>
-      {isOpen && <div>
+      {isOpen && <div
+        className="absolute top-full border rounded p-3 shadow bg-white w-full"
+      >
         {renderedOptions}
       </div>}
-    </>
+    </div>
   )
 }
 
