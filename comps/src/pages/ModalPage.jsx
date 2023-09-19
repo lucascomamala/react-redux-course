97a946bd56b05e72d1398d90df0ce45fe0efafcf
Lucas <lucascomamala@gmail.com>
2023-09-19 01:45:45 -0600

    233 - Toggling Visibility

--- comps/src/pages/ModalPage.jsx
+++ comps/src/pages/ModalPage.jsx
@@ -1,9 +1,19 @@
+import { useState } from 'react'
+
 import Modal from '../components/Modal'
+import Button from '../components/Button'
 
 const ModalPage = () => {
+  const [showModal, setShowModal] = useState(false)
+
+  const handleClick = () => {
+    setShowModal(true)
+  }
+
   return (
     <div>
-      <Modal />
+      <Button onClick={handleClick} primary className='mb-3'>Open Modal</Button>
+      {showModal && <Modal>Modal Content</Modal>}
     </div>
   )
 }
