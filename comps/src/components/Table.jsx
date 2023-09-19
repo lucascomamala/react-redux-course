e7044caf241512c32c854c63864be93dccafea80
Lucas <lucascomamala@gmail.com>
2023-09-19 12:09:30 -0600

    246 - Better Styling

--- comps/src/components/Table.jsx
+++ comps/src/components/Table.jsx
@@ -1,18 +1,20 @@
 const Table = ({ data }) => {
   const rows = data.map((item, index) => {
     return (
-      <tr key={index}>
-        <td className={item.color}>{item.name}</td>
-        <td className={item.color}>{item.color}</td>
-        <td className={item.color}>{item.score}</td>
+      <tr className="border-b" key={index}>
+        <td className='p-3'>{item.name}</td>
+        <td className='p-3'>
+          <div className={`p-3 m-2 ${item.color}`} />
+        </td>
+        <td className='p-3'>{item.score}</td>
       </tr>
     )
   })
-  
+
   return (
-    <table>
+    <table className="table-auto border-spacing-2">
       <thead>
-        <tr>
+        <tr className="border-b-2">
           <th>Fruit</th>
           <th>Color</th>
           <th>Score</th>
