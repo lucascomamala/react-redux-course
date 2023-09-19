e8d3c6fd0ac5bedd6a5fae3b65d22248f410de51
Lucas <lucascomamala@gmail.com>
2023-09-19 11:44:33 -0600

    238 - Customizing the Modal

--- comps/src/pages/ModalPage.jsx
+++ comps/src/pages/ModalPage.jsx
@@ -6,10 +6,15 @@ import Button from '../components/Button'
 const ModalPage = () => {
   const [showModal, setShowModal] = useState(false)
 
+  const actionBar = <Button onClick={() => setShowModal(false)} primary>I accept</Button>
+  const modal = <Modal onClose={() => setShowModal(false)} actionBar={actionBar}>
+    <p>Here is an important agreement for you to accept</p>
+  </Modal>
+
   return (
     <div>
       <Button onClick={() => setShowModal(true)} primary className='mb-3'>Open Modal</Button>
-      {showModal && <Modal onClose={() => setShowModal(false)}>Modal Content</Modal>}
+      {showModal && modal}
     </div>
   )
 }
