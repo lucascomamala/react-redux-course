c5fa350fbf1b4d17d1c692597ad1c6ac74b0d6e7
Lucas <lucascomamala@gmail.com>
2023-09-19 01:47:53 -0600

    234 - At First Glance Easy

--- comps/src/components/Modal.jsx
+++ comps/src/components/Modal.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Modal = () => {
   return (
-    <div>Modal</div>
+    <div>
+      <div className='absolute inset-0 bg-gray-300 opacity-80'></div>
+      <div className='absolute inset-40 p-10 bg-white'>
+        I'm a modal
+      </div>
+    </div>
   )
 }
 
