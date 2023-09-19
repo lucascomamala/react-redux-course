955eb64f0280aa61c7befda2ef38dc32b5b70e97
Lucas <lucascomamala@gmail.com>
2023-09-19 00:19:38 -0600

    226 - Handling Control and Command Keys

--- comps/src/components/Link.jsx
+++ comps/src/components/Link.jsx
@@ -6,12 +6,13 @@ const Link = ({ path, children }) => {
   const { navigate } = useContext(NavigationContext)
 
   const handleClick = (e) => {
+    if (e.metaKey || e.ctrlKey) return
     e.preventDefault()
     navigate(path)
   }
 
   return (
-    <a onClick={handleClick}>
+    <a onClick={handleClick} href={path}>
       {children}
     </a>
   )
