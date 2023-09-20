f0a48d878e652e7623c4e9695ebac8b001cde8d3
Lucas <lucascomamala@gmail.com>
2023-09-20 14:20:08 -0600

    323 - Adding the Form Slice

---
+++ cars/src/store/slices/formSlice.js
@@ -0,0 +1,20 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+const formSlice = createSlice({
+  name: 'form',
+  initialState: {
+    name: '',
+    cost: 0,
+  },
+  reducers: {
+    setName: (state, action) => {
+      state.name = action.payload
+    },
+    setCost: (state, action) => {
+      state.cost = action.payload
+    }
+  }
+})
+
+export const {setName, setCost} = formSlice.actions
+export const formReducer = formSlice.reducer
