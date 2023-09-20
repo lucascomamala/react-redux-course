9330749d9d79b34ce90be9e59c964aba3bc136fb
Lucas <lucascomamala@gmail.com>
2023-09-19 19:09:53 -0600

    284 - More on the Form

--- comps/src/pages/CounterPage.jsx
+++ comps/src/pages/CounterPage.jsx
@@ -5,30 +5,33 @@ import Button from '../components/Button'
 
 const CounterPage = ({ initialCount }) => {
   const [count, setCount] = useState(initialCount)
+  const [step, setStep] = useState(1)
 
   const increment = () => {
-    setCount(count + 1)
+    setCount(count + step)
   }
 
   const decrement = () => {
-    setCount(count - 1)
+    setCount(count - step)
   }
 
   return (
     <Panel className='m-3'>
       <h1 className='text-lg'>Count is {count}</h1>
-      <div className='flex flex-row'>
-        <Button onClick={increment}>
-          Increment
-        </Button>
+      <div className='flex flex-row space-x-3'>
         <Button onClick={decrement}>
           Decrement
+        </Button>
+        <Button onClick={increment}>
+          Increment
         </Button>
       </div>
       <form>
         <label>By how much</label>
         <input
           type='number'
+          value={step}
+          onChange={e => setStep(parseInt(e.target.value) || 0)}
           className='p-1 m-3 bg-gray-50 border border-gray-300'
         />
       </form>
