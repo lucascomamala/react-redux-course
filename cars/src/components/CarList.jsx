732fb96360b7e0107e91d7cbfefd8901288c6f76
Lucas <lucascomamala@gmail.com>
2023-09-20 14:51:18 -0600

    330 - Listing the Records

--- cars/src/components/CarList.jsx
+++ cars/src/components/CarList.jsx
@@ -1,9 +1,24 @@
-import {useSelector} from 'react-redux'
+import { useSelector } from 'react-redux'
 
 const CarList = () => {
   const cars = useSelector(state => state.cars.data)
+
+  const renderedCars = cars.map(car => (
+    <div key={car.id} className="card">
+      <p>
+        <strong>{car.name}</strong> - ${car.cost}
+      </p>
+      <button className="button is-danger" onClick={() => { }}>
+        Delete
+      </button>
+    </div>
+  ))
+
   return (
-    <div>CarList</div>
+    <div className='car-list'>
+      {renderedCars}
+      <br />
+    </div>
   )
 }
 
