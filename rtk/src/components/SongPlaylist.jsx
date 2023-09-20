e83f9bf0e9fe5fccf325cbfd27248c51de585490
Lucas <lucascomamala@gmail.com>
2023-09-20 02:49:04 -0600

    305 - Accessing State in a Component

--- rtk/src/components/SongPlaylist.jsx
+++ rtk/src/components/SongPlaylist.jsx
@@ -1,14 +1,13 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { createRandomSong } from "../data";
 
 import { addSong, removeSong } from '../store'
+import { store } from '../store'
 
 function SongPlaylist() {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
-  // To Do:
-  // Get list of songs
-  const songPlaylist = [];
+  const songPlaylist = useSelector((state) => state.songs);
 
   const handleSongAdd = (song) => {
     dispatch(addSong(song))
