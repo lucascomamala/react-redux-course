10d19ce8aee78fe941a663043bba1e3a5d05bdb9
Lucas <lucascomamala@gmail.com>
2023-09-20 23:25:07 -0600

    356 - Using Loading State

--- media/src/components/UsersList.jsx
+++ media/src/components/UsersList.jsx
@@ -1,17 +1,30 @@
-import {useEffect} from 'react'
-import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 import { fetchUsers } from '../store'
 
 const UsersList = () => {
   const dispatch = useDispatch()
 
+  const { isLoading, data, error } = useSelector((state) => state.users)
+
   useEffect(() => {
     dispatch(fetchUsers())
   }, [dispatch])
-  
+
+  if (isLoading) {
+    return (
+      <div>Loading...</div>
+    )
+  }
+
+  if (error) {
+    console.log(error)
+    return <div>Error fetching data...</div>
+  }
+
   return (
-    <div>UsersList</div>
+    <div>{data.length}</div>
   )
 }
 
