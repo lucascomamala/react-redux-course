d3452818dc54075fba9da83cc11c64f4fe9161c3
Lucas <lucascomamala@gmail.com>
2023-09-22 06:02:33 -0600

    378 - Wrapping Up the ExpandablePanel

--- media/src/components/index.js
+++ media/src/components/index.js
@@ -1,9 +1,13 @@
 import Button from './ui/Button'
 import Skeleton from './ui/Skeleton'
 import Panel from './ui/Panel'
+import ExpandablePanel from './ui/ExpandablePanel'
 
 import UsersList from './users/UsersList'
 import UserListItem from './users/UserListItem'
+
+import AlbumsList from './albums/AlbumsList'
+import AlbumListItem from './albums/AlbumListItem'
 
 export {
   Button,
@@ -11,4 +15,7 @@ export {
   Panel,
   UsersList,
   UserListItem,
+  AlbumsList,
+  AlbumListItem,
+  ExpandablePanel
 }
--- media/src/components/users/UserListItem.jsx
+++ media/src/components/users/UserListItem.jsx
@@ -3,30 +3,31 @@ import { GoTrash } from 'react-icons/go'
 import { useThunk } from '../../hooks/useThunk'
 import { removeUser } from '../../store'
 
-import { Button } from '../'
+import { Button, ExpandablePanel } from '../'
 
 const UserListItem = ({ user }) => {
   const [doRemoveUser, isRemovingUser, removingUserError] = useThunk(removeUser)
 
+  const header = <>
+    <Button
+      className='mr-3'
+      loading={isRemovingUser}
+      onClick={() => {
+        console.log(user)
+        doRemoveUser(user)
+      }}
+    >
+      <GoTrash />
+    </Button>
+    {removingUserError && console.log(removingUserError)}
+    {user.name}
+  </>
+
   return (
-    <div className='mb-2 border rounded'>
-      <div className='flex p-2 justify-between items-center cursor-pointer'>
-        <div className='flex flex-row items-center justify-between'>
-          <Button
-            className='mr-3'
-            loading={isRemovingUser}
-            onClick={() => {
-              console.log(user)
-              doRemoveUser(user)
-            }}
-          >
-            <GoTrash />
-          </Button>
-          {removingUserError && console.log(removingUserError)}
-          {user.name}
-        </div>
-      </div>
-    </div>
+    <ExpandablePanel header={header}>
+      CONTENT
+    </ExpandablePanel>
+
   )
 }
 
