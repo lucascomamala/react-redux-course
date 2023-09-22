00e6c536c4e2782397b4af0fd504a66b143c8aaf
Lucas <lucascomamala@gmail.com>
2023-09-21 18:52:04 -0600

    371 - Updating the Slice

--- media/src/store/slices/usersSlice.js
+++ media/src/store/slices/usersSlice.js
@@ -2,6 +2,7 @@ import { createSlice } from '@reduxjs/toolkit'
 
 import { fetchUsers } from '../thunks/fetchUsers'
 import { addUser } from '../thunks/addUser'
+import { removeUser } from '../thunks/removeUser'
 
 const usersSlice = createSlice({
   name: 'users',
@@ -32,6 +33,19 @@ const usersSlice = createSlice({
       state.data.push(action.payload.data)
     })
     builder.addCase(addUser.rejected, (state, action) => {
+      state.isLoading = false
+      state.error = action.error
+    })
+    // Remove user
+    builder.addCase(removeUser.pending, (state, action) => {
+      state.isLoading = true
+    })
+    builder.addCase(removeUser.fulfilled, (state, action) => {
+      state.isLoading = false
+      // FIX !!!
+      console.log(action)
+    })
+    builder.addCase(removeUser.rejected, (state, action) => {
       state.isLoading = false
       state.error = action.error
     })
