29e85c82955147a64e22ab4d891d28410f685ef7
Lucas <lucascomamala@gmail.com>
2023-09-21 19:06:57 -0600

    373 - Deleting the User

--- media/src/components/users/UserListItem.jsx
+++ media/src/components/users/UserListItem.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { GoTrash } from 'react-icons/go'
 
-const UserListItem = ({user}) => {
+import { useThunk } from '../../hooks/useThunk'
+import { removeUser } from '../../store'
+
+import { Button } from '../'
+
+const UserListItem = ({ user }) => {
+  const [doRemoveUser, isRemovingUser, removingUserError] = useThunk(removeUser)
+
   return (
     <div className='mb-2 border rounded'>
       <div className='p-2 justify-between items-center cursor-pointer'>
+        <Button loading={isRemovingUser} onClick={() => doRemoveUser(user)}>
+          <GoTrash />
+        </Button>
+        {removingUserError && console.log(user)}
         {user.name}
       </div>
     </div>
--- media/src/store/thunks/removeUser.js
+++ media/src/store/thunks/removeUser.js
@@ -1,8 +1,8 @@
 import { createAsyncThunk } from "@reduxjs/toolkit"
 import axios from "axios"
 
-const removeUser = createAsyncThunk('users/removeUser', async (id) => {
-  const response = await axios.delete(`http://localhost:3005/users/${id}`)
+const removeUser = createAsyncThunk('users/removeUser', async (user) => {
+  const response = await axios.delete(`http://localhost:3005/users/${user.id}`)
   // FIX !!!
   return response.data
 })
