28c50cb0457218761b6456ef06a6d812f461b4b0
Lucas <lucascomamala@gmail.com>
2023-09-23 00:21:32 -0600

    396 - Adding a Pause for Testing

--- media/src/components/albums/AlbumsList.jsx
+++ media/src/components/albums/AlbumsList.jsx
@@ -8,7 +8,7 @@ const AlbumsList = ({ user }) => {
   let content
 
   if (isLoading) {
-    content = <Skeleton times={3} />
+    content = <Skeleton className='h-10 w-full' times={3} />
   } else if (error) {
     content = <div>{error}</div>
   } else if (data) {
