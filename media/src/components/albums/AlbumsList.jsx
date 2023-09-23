96ba8ff853c0ab3963ca1aba5ecff7df136ab918
Lucas <lucascomamala@gmail.com>
2023-09-22 18:19:34 -0600

    387 - Rendering the List

--- media/src/components/albums/AlbumsList.jsx
+++ media/src/components/albums/AlbumsList.jsx
@@ -1,11 +1,31 @@
 import { useGetAlbumsQuery } from "../../store"
-const AlbumsList = ({user}) => {
-  const { data: albums, error, isLoading } = useGetAlbumsQuery(user)
 
-  console.log(albums,error,isLoading)
+import { Skeleton, ExpandablePanel, Button } from '../'
+const AlbumsList = ({ user }) => {
+  const { data, error, isLoading } = useGetAlbumsQuery(user)
+
+  let content
+
+  if (isLoading) {
+    content = <Skeleton times={3} />
+  } else if (error) {
+    content = <div>{error}</div>
+  } else if (data) {
+    content = data.map(album => {
+      const header = album.title
+      return <ExpandablePanel key={album.id} header={header}>
+        list of photos
+      </ExpandablePanel>
+    })
+  }
 
   return (
-    <div>AlbumsList {user.name}</div>
+    <div>
+      <div>AlbumsList {user.name}</div>
+      <div>
+        {content}
+      </div>
+    </div>
   )
 }
 
