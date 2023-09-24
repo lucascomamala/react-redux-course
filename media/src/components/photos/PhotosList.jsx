2da99cacb15124e041ae27b7dc94d1c1511f3e13
Lucas <lucascomamala@gmail.com>
2023-09-23 18:34:07 -0600

    406 - Creating the Photo

--- media/src/components/photos/PhotosList.jsx
+++ media/src/components/photos/PhotosList.jsx
@@ -1,10 +1,24 @@
-import { PhotosListItem } from "../"
-import { useGetPhotosQuery } from "../../store"
+import { PhotosListItem, Button } from "../"
+import { useGetPhotosQuery, useAddPhotoMutation } from "../../store"
 
 const PhotosList = ({ album }) => {
   const { data: photos, isLoading } = useGetPhotosQuery(album)
+  const [addPhoto, addPhotoResults] = useAddPhotoMutation()
+
   return (
-    <div>PhotosList</div>
+    <div>
+      <div className="m-2 flex flex-row items-center justify-between">
+        <h3 className="text-lg font-bold">
+          Photos In {album.title}
+        </h3>
+        <Button
+          onClick={() => addPhoto(album)}
+          loading={addPhotoResults.isLoading}
+        >
+          + Add Photo
+        </Button>
+      </div>
+    </div>
   )
 }
 
